14e8095b06272886b82e6ae2b5010bf798db794c
Markus Tervonen <markus.tervonen@hotmail.com>
2025-05-19 16:10:44 +0300

    teht 1.8

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -2,24 +2,41 @@ import { useState } from 'react'
 
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
 
+const Statistics = (props) => {
+  const good = props.stats[0]
+  const neutral = props.stats[1]
+  const bad = props.stats[2]
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all
+  
+  return (
+  <div>
+    <h1>Statistics</h1>
+    <p>Good {good}</p>
+    <p>Neutral {neutral}</p>
+    <p>Bad {bad}</p> 
+    <p>All {all}</p>
+    <p>Average {average}</p>
+    <p>Positive {positive}</p>
+  </div>
+  )
+}
+
 const App = () => {
-  // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const all = bad + neutral + good
-  const average = (good - bad) / all
-  const positive = good / all
 
-const handleClickGood = () => {
-  setGood(good + 1)
-}
-const handleClickNeutral = () => {
-setNeutral(neutral + 1)
-}
-const handleClickBad = () => {
-setBad(bad + 1)
-}
+  const handleClickGood = () => {
+    setGood(good + 1)
+  }
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const handleClickBad = () => {
+    setBad(bad + 1)
+  }
 
   return (
     <div>
@@ -27,13 +44,7 @@ setBad(bad + 1)
       <Button onClick={handleClickGood} text='Good' />
       <Button onClick={handleClickNeutral} text='Neutral' />
       <Button onClick={handleClickBad} text='Bad' />
-      <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p> 
-      <p>All {all}</p>
-      <p>Average {average}</p>
-      <p>Positive {positive}</p>
+      <Statistics stats={[good, neutral, bad]} />
     </div>
   )
 }
