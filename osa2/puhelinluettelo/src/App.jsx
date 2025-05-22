61be90e200abd25f6785e5072c36aae361b7f464
Markus Tervonen <markus.tervonen@hotmail.com>
2025-05-22 12:18:53 +0300

    teht 2.16

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -2,13 +2,27 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import personService from './services/persons'
 
+// TODO: Henkilön lisäys, poisto, numeron muutos uudella virheilmoituksella
+
 function alertAlreadyAdded(newName) {
   alert(`${newName} is already added to the phonebook`)
 }
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
 const Person = (props) => {
   console.log(props)
-  return(
+  return (
     <li>
       {props.name} {props.number} {" "}
       <button onClick={() => props.alertDeletePerson(props)}>delete</button>
@@ -20,25 +34,25 @@ const ShowList = (props) => {
 
   console.log(props)
 
-  if(props.results === '') {
-    return(
-        <ul>
-          {props.persons.map(person => 
-            <Person key={person.id} name={person.name} number={person.number} id={person.id} alertDeletePerson={props.alertDeletePerson} />
+  if (props.results === '') {
+    return (
+      <ul>
+        {props.persons.map(person =>
+          <Person key={person.id} name={person.name} number={person.number} id={person.id} alertDeletePerson={props.alertDeletePerson} />
         )}
-        </ul>
+      </ul>
     )
   } else {
     let searchResults = props.persons.filter(person => person.name.toLowerCase().includes(props.results))
     console.log(searchResults)
     console.log(props)
-      return(  
-          <ul>
-            {searchResults.map(person => 
-              <Person key={person.id} name={person.name} number={person.number} id={person.id} alertDeletePerson={props.alertDeletePerson} />
-          )}
-          </ul>
-      )
+    return (
+      <ul>
+        {searchResults.map(person =>
+          <Person key={person.id} name={person.name} number={person.number} id={person.id} alertDeletePerson={props.alertDeletePerson} />
+        )}
+      </ul>
+    )
   }
 }
 
@@ -47,14 +61,15 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchContact, setSearchContact] = useState('')
+  const [errorMessage, setErrorMessage] = useState(null)
   const [searchResult, setSearchResult] = useState('')
   const url = 'http://localhost:3001/persons'
 
   useEffect(() => {
     personService
       .getAll()
-        .then(initialPersons => {
-          console.log(initialPersons)
+      .then(initialPersons => {
+        console.log(initialPersons)
         setPersons(initialPersons)
       })
   }, [])
@@ -65,6 +80,12 @@ const App = () => {
         console.log('User deleted')
         setPersons(persons.filter(n => n.id !== props.id))
       })
+      setErrorMessage(
+        `Person '${props.name}' has been deleted from the phonebook.`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     } else {
       console.log("no")
     }
@@ -72,38 +93,56 @@ const App = () => {
 
   const handleNameSubmission = (event) => {
     event.preventDefault()
-    if((persons.map(person => person.name).includes(newName)) != true) {
+    if ((persons.map(person => person.name).includes(newName)) != true) {
       let nameObject = {
         name: newName,
         number: newNumber,
-        id: String(persons.length+1)
+        id: String(persons.length + 1)
       }
       personService
         .create(nameObject)
-          .then(returnedPersons => {
-            setPersons(persons.concat(returnedPersons))   
-            setNewNumber("")
-            setNewName("")
-          })              
-    } else if((persons.map(person => person.name).includes(newName)) == true) {
+        .then(returnedPersons => {
+          setPersons(persons.concat(returnedPersons))
+          setNewNumber("")
+          setNewName("")
+        })
+      setErrorMessage(
+        `Person '${newName}' has been added to the phonebook.`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    } else if ((persons.map(person => person.name).includes(newName)) == true) {
       if (window.confirm(`${newName} is already added to the phonebook, replace the old number with a new one?`)) {
         console.log('yes')
         let oldObject = persons.find(({ name }) => name === newName)
-        let newObject = { ...oldObject, number: newNumber}
+        let newObject = { ...oldObject, number: newNumber }
 
         console.log(oldObject)
         personService
           .update(oldObject.id, newObject)
-            .then(returnedPersons => {
-              setPersons(persons.map(p => (p.id !== oldObject.id ? p : returnedPersons)))   
-              setNewNumber("")
-              setNewName("")          
-        })
+          .then(returnedPersons => {
+            setPersons(persons.map(p => (p.id !== oldObject.id ? p : returnedPersons)))
+            setNewNumber("")
+            setNewName("")
+          })
+        setErrorMessage(
+          `The number for '${newName}' has been updated.`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
       } else {
         console.log('declined')
       }
     } else {
-      alertAlreadyAdded(newName)
+      //alertAlreadyAdded(newName)
+      setErrorMessage(
+        `Person '${newName}' already exists in the phonebook.`
+      )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
     }
   }
   const handleFieldChange = (event) => {
@@ -121,19 +160,20 @@ const App = () => {
 
   return (
     <div>
+      <Notification message={errorMessage} />
       <h2>Phonebook</h2>
-          <div>Filter phonebook with <input search={searchContact} onChange={handleSearchFieldChange}/></div>
-        <div>
-          <h1>Add a new contact</h1>
-            <form onSubmit={handleNameSubmission}>
-              <div>name: <input value={newName} onChange={handleFieldChange}/></div>
-              <div>number: <input number={newNumber} onChange={handleNumberFieldChange}/></div>            
-            <button type="submit">save</button>
-          </form>   
-        </div>
+      <div>Filter phonebook with <input search={searchContact} onChange={handleSearchFieldChange} /></div>
+      <div>
+        <h1>Add a new contact</h1>
+        <form onSubmit={handleNameSubmission}>
+          <div>name: <input value={newName} onChange={handleFieldChange} /></div>
+          <div>number: <input number={newNumber} onChange={handleNumberFieldChange} /></div>
+          <button type="submit">save</button>
+        </form>
+      </div>
       <h2>Numbers</h2>
       <div>
-        <ShowList persons={persons} results={searchContact} alertDeletePerson={alertDeletePerson}/>
+        <ShowList persons={persons} results={searchContact} alertDeletePerson={alertDeletePerson} />
       </div>
     </div>
   )
