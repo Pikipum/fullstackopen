678cb24d2d72d4e2a7113cc024d5d99de52cb61b
Markus Tervonen <markus.tervonen@hotmail.com>
2025-05-26 11:58:44 +0300

    teht 3.16

--- osa3/puhelinluettelobackend/index.js
+++ osa3/puhelinluettelobackend/index.js
@@ -2,8 +2,27 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
 app.use(express.json())
 app.use(express.static('dist'))
+app.use(requestLogger)
 
 const morgan = require('morgan')
 
@@ -54,15 +73,17 @@ const randomId = () => {
     return Math.floor(Math.random() * 10000)
 }
 
-app.get('/', (request, response) => {
+app.get('/', (request, response, next) => {
     response.send('<h1>Hello World!</h1>')
     console.log('Hello world sent!')
+        .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
+app.get('/api/persons', (request, response, next) => {
     Person.find({}).then(persons => {
         response.json(persons)
     })
+        .catch(error => next(error))
 })
 /*
 response.json(phonebook.persons)
@@ -70,54 +91,56 @@ console.log('Phonebook sent!')
 */
 
 
-app.get('/info', (request, response) => {
+app.get('/info', (request, response, next) => {
     response.send(`<p>Phonebook has info for ${phonebook.persons.length} people</p><p>${Date()}</p>`)
     console.log('Phonebook sent!')
+        .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
     Person.findById(request.params.id).then(person => {
         if (person) {
             response.json(person)
         } else {
             response.status(404).end()
         }
-    })
+    })/*
         .catch(error => {
             console.log(error)
             response.status(400).send({ error: 'malformatted id' })
-        })
-    /*
-    const id = request.params.id
-    let person = phonebook.persons.find(person => person.id == id)
-    if (person) {
-        response.json(person)
-        console.log('Contact sent')
-    } else {
-        return response.status(404).json({
-            error: 'content missing'
-        }).end()
-    }
-        */
+            */
+        .catch(error => next(error))
 })
+/*
+const id = request.params.id
+let person = phonebook.persons.find(person => person.id == id)
+if (person) {
+    response.json(person)
+    console.log('Contact sent')
+} else {
+    return response.status(404).json({
+        error: 'content missing'
+    }).end()
+}
+    */
 
-app.delete('/api/persons/:id', (request, response) => {
+
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id).then(person => {
         response.status(204).end()
     })
-    .catch(error => {
-        console.log(error)
-    })
-
-    /*
-    const id = request.params.id
- 
-    phonebook.persons = phonebook.persons.filter(person => person.id !== id)
-    response.status(204).end()
-    */
+        .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+/*
+const id = request.params.id
+ 
+phonebook.persons = phonebook.persons.filter(person => person.id !== id)
+response.status(204).end()
+*/
+
+
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
@@ -142,12 +165,20 @@ app.post('/api/persons', (request, response) => {
     addPerson.save().then(savedPerson => {
         response.json(savedPerson)
     })
+        .catch(error => next(error))
     /*
     phonebook.persons = phonebook.persons.concat(addPerson)
     response.json(addPerson)
     */
 
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 
