bda5afa28afe6e5202eaa354c6fd6ef1c3fceda8
Markus Tervonen <markus.tervonen@hotmail.com>
2025-05-26 19:47:52 +0300

    teht 3.20 ja 3.21

--- osa3/puhelinluettelobackend/models/person.js
+++ osa3/puhelinluettelobackend/models/person.js
@@ -21,7 +21,13 @@ const personSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        required: true
+        required: true,
+        minlength: 8,
+        validate: {
+            validator: function (v) {
+                return /\d{2,3}-\d{5,}/.test(v)
+            }
+        }
     }
 })
 
