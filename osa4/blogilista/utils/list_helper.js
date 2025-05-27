a43cdd675f487aef6dbdb0719718cdc4f87f8ae0
Markus Tervonen <markus.tervonen@hotmail.com>
2025-05-27 19:48:00 +0300

    teht 4.4

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -1,7 +1,17 @@
 const dummy = (blogs) => {
-  return 1
+    return 1
+}
+
+const totalVotes = (blogs) => {
+    let total = 0
+    blogs.map(blog => {
+        total += blog.votes
+    })
+
+    return total
 }
 
 module.exports = {
-  dummy
+    dummy,
+    totalVotes
 }
