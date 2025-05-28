4a2b5a97654dbe568f37d46ccf27ffb5b7d40a2a
Markus Tervonen <markus.tervonen@hotmail.com>
2025-05-28 13:51:39 +0300

    teht 4.9

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -1,16 +1,28 @@
-const { test, after } = require('node:test')
+const { test, after, beforeEach } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const assert = require('node:assert')
+const Blog = require('../models/blog')
+const helper = require('./test_helper')
 
 const api = supertest(app)
 
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(helper.initialBlogs)
+})
+
 test('right amount of blogs is returned', async () => {
     const response = await api.get('/api/blogs')
-    console.log(response.body.length)
 
-    assert.strictEqual(response.body.length, 1)
+    assert.strictEqual(response.body.length, 6)
+})
+
+test('blogs have correct id field', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(Object.keys(response.body[0]).includes('id'), true)
 })
 
 after(async () => {
