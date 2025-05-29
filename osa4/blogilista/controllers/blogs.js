9a1105bae82223f531035e229e2edc9cfdf06261
Markus Tervonen <markus.tervonen@hotmail.com>
2025-05-29 20:04:59 +0300

    teht 4.19

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,6 +2,15 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const logger = require('../utils/logger')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user')
@@ -14,20 +23,26 @@ blogsRouter.post('/', async (request, response) => {
   if (!blog.votes) {
     blog['votes'] = 0
   }
-  const user = await User.findById(blog.user)
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
   if (!user) {
-    return response.status(400).json({ error: 'User not found' })
+    return response.status(400).json({ error: 'UserID missing or not valid' })
   }
 
-  blog.user = user
+  blog.user = user._id
 
   logger.info(request.body)
   logger.info(blog)
 
   try {
-    user.blogs = user.blogs.concat(blog.id)
+    const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
-    const result = await blog.save()
     response.status(201).json(result)
   } catch (err) {
     response.status(400).json(err)
