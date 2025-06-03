56ed783cc96aad1a054b7db98ff59c025e7b0e47
Markus Tervonen <markus.tervonen@hotmail.com>
2025-06-03 13:13:39 +0300

    teht 5.7

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,43 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const blogStyle = {
+    paddingTop: 3,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 1
+  }
+
+  if (visible) {
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author} <button onClick={toggleVisibility}>Hide</button>
+          <div>
+            {blog.blogUrl}
+          </div>
+          <div>
+            Votes: {blog.votes} <button>Like</button>
+          </div>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>Show</button>
+      </div>
+    )
+  }
+
+
+}
 
 export default Blog
