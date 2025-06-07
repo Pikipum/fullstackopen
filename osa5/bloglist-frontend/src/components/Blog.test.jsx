eb6f521dc7e31421c54e93502faad131b4612f00
Markus Tervonen <markus.tervonen@hotmail.com>
2025-06-07 13:24:51 +0300

    teht 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,11 +1,10 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
-import { beforeEach, describe, expect } from 'vitest'
-
+import { beforeEach, describe, expect, test, vi } from 'vitest'
 
 describe('rendering blogs', () => {
     let container
-
     beforeEach(() => {
         const blog = {
             id: 'aksd903jgfjae2',
@@ -29,16 +28,63 @@ describe('rendering blogs', () => {
             'John Tester'
         )
     })
-    test('doesnt render url', () => {
+    test('doesnt render url by default', () => {
         const div = container.querySelector('.blog')
         expect(div).not.toHaveTextContent(
             'testing.com'
         )
     })
-    test('doesnt render votes', () => {
+    test('doesnt render votes by default', () => {
         const div = container.querySelector('.blog')
         expect(div).not.toHaveTextContent(
             9
+        )
+    })
+})
+describe('button pressed', async () => {
+    let container
+    beforeEach(() => {
+        const blog = {
+            id: 'aksd903jgfjae2',
+            title: 'Test title',
+            author: 'John Tester',
+            blogUrl: 'testing.com',
+            votes: 965,
+            user: {
+                name: 'John Blogger'
+            }
+        }
+        const mockHandler = vi.fn()
+        container = render(<Blog blog={blog} toggleVisibility={mockHandler} />).container
+    })
+    test('url renders when show button pressed', async () => {
+        const user = userEvent.setup()
+        const button = screen.getByText('Show')
+        await user.click(button)
+        const div = container.querySelector('.blog')
+
+        expect(div).toHaveTextContent(
+            'testing.com'
+        )
+    })
+    test('votes renders when show button pressed', async () => {
+        const user = userEvent.setup()
+        const button = screen.getByText('Show')
+        await user.click(button)
+        const div = container.querySelector('.blog')
+
+        expect(div).toHaveTextContent(
+            965
+        )
+    })
+    test('user renders when show button pressed', async () => {
+        const user = userEvent.setup()
+        const button = screen.getByText('Show')
+        await user.click(button)
+        const div = container.querySelector('.blog')
+
+        expect(div).toHaveTextContent(
+            'John Blogger'
         )
     })
 })
