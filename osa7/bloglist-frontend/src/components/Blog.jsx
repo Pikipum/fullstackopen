8c9dab082e999760b4886539ab14f4744b81e70b
Markus Tervonen <markus.tervonen@hotmail.com>
2025-08-18 14:01:44 +0300

    copied bloglist backend and frontend to part 7 directory

---
+++ osa7/bloglist-frontend/src/components/Blog.jsx
@@ -0,0 +1,109 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const Blog = ({ blog, updateBlog, removeBlog, setErrorMessage, user, name }) => {
+    const [visible, setVisible] = useState(false)
+
+    const toggleVisibility = () => {
+        setVisible(!visible)
+    }
+
+    const removeBlogConfirm = async (event) => {
+        if (window.confirm(`Do you want to remove ${blog.title} by ${blog.author}`)) {
+            const response = await removeBlog({
+                _id: blog.id
+            })
+        }
+        return null
+    }
+
+    const voteBlog = async (event) => {
+        event.preventDefault()
+        try {
+            const response = await updateBlog({
+                _id: blog.id,
+                title: blog.title,
+                author: blog.author,
+                blogUrl: blog.blogUrl,
+                votes: blog.votes + 1,
+                voteUpdate: true
+            }
+            )
+
+            console.log(response)
+            setErrorMessage(`You liked "${blog.title}"`)
+            setTimeout(() => setErrorMessage(null), 5000)
+        } catch (error) {
+            console.log(error)
+            setErrorMessage('Could not submit like.', error)
+            setTimeout(() => setErrorMessage(null), 5000)
+        }
+    }
+
+    const blogStyle = {
+        paddingTop: 3,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 1
+    }
+
+    if (visible && blog.user && name === blog.user.name) {
+        return (
+            <div className="blog" style={blogStyle}>
+                <div>
+                    {blog.title} {blog.author} <button onClick={toggleVisibility}>Hide</button>
+                    <div>
+                        {blog.blogUrl}
+                    </div>
+                    <div>
+                        Votes: {blog.votes} <button onClick={voteBlog}>Like</button>
+                    </div>
+                    <div>
+                        {blog.user.name}
+                    </div>
+                    <div>
+                        <button onClick={removeBlogConfirm}>Remove</button>
+                    </div>
+                </div>
+            </div>
+        )
+    } else if (visible && blog.user && name !== blog.user.name) {
+        return (
+            <div className="blog" style={blogStyle}>
+                <div>
+                    {blog.title} {blog.author} <button onClick={toggleVisibility}>Hide</button>
+                    <div>
+                        {blog.blogUrl}
+                    </div>
+                    <div>
+                        Votes: {blog.votes} <button onClick={voteBlog}>Like</button>
+                    </div>
+                    <div>
+                        {blog.user.name}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div className="blog" style={blogStyle}>
+                {blog.title} {blog.author} <button onClick={toggleVisibility}>Show</button>
+            </div>
+        )
+    }
+
+
+}
+
+Blog.propTypes = {
+    blog: PropTypes.object.isRequired,
+    updateBlog: PropTypes.func.isRequired,
+    removeBlog: PropTypes.func.isRequired,
+    setErrorMessage: PropTypes.func.isRequired,
+    user: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired
+}
+
+export default Blog
---
+++ osa7/bloglist-frontend/src/components/CreateBlogForm.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import PropTypes from 'prop-types'
+
+const CreateBlogForm = ({ addBlog, blogFormRef, setErrorMessage }) => {
+    const [newTitle, setNewTitle] = useState('')
+    const [newAuthor, setNewAuthor] = useState('')
+    const [newUrl, setNewUrl] = useState('')
+
+    const handleSubmit = async (event) => {
+        event.preventDefault()
+        try {
+            await addBlog({
+                title: newTitle,
+                author: newAuthor,
+                blogUrl: newUrl
+            })
+            setNewTitle('')
+            setNewAuthor('')
+            setNewUrl('')
+
+            setErrorMessage(`Blog "${newTitle}" submitted successfully!`)
+            setTimeout(() => setErrorMessage(null), 5000)
+            if (blogFormRef && blogFormRef.current) blogFormRef.current.toggleVisibility()
+        } catch (error) {
+            setErrorMessage('Could not submit blog.')
+            setTimeout(() => setErrorMessage(null), 5000)
+        }
+    }
+
+    return (
+        <div className="blogDiv">
+            <h2>Create new blog</h2>
+            <form onSubmit={handleSubmit}>
+                <div>Title: <input name="title" value={newTitle} placeholder='blog title' id='title-input' onChange={e => setNewTitle(e.target.value)} /></div>
+                <div>Author: <input name="author" value={newAuthor} placeholder='author name' id='author-input' onChange={e => setNewAuthor(e.target.value)} /></div>
+                <div>URL: <input name="blogUrl" value={newUrl} placeholder='blog url' id='url-input' onChange={e => setNewUrl(e.target.value)} /></div>
+                <button type='submit'>Create</button>
+            </form>
+        </div>
+    )
+}
+
+CreateBlogForm.propTypes = {
+    addBlog: PropTypes.func.isRequired,
+    setErrorMessage: PropTypes.func.isRequired
+}
+
+export default CreateBlogForm
