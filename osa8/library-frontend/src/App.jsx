a378b4334c359a220ab5ebe673829a67acffb233
Markus Tervonen <markus.tervonen@hotmail.com>
2025-09-10 11:16:09 +0300

    teht 8.8

---
+++ osa8/library-frontend/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Authors from "./components/Authors";
+import Books from "./components/Books";
+import NewBook from "./components/NewBook";
+import { useQuery } from "@apollo/client/react";
+import { ALL_AUTHORS } from "./queries";
+// import { gql, useQuery } from '@apollo/client/react'
+
+const App = () => {
+  const [page, setPage] = useState("authors");
+
+  const result = useQuery(ALL_AUTHORS)
+
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage("authors")}>authors</button>
+        <button onClick={() => setPage("books")}>books</button>
+        <button onClick={() => setPage("add")}>add book</button>
+      </div>
+
+      <Authors show={page === "authors"} />
+
+      <Books show={page === "books"} />
+
+      <NewBook show={page === "add"} />
+    </div>
+  );
+};
+
+export default App;
