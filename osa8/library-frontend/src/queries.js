820fbf19c10a7296c4af459d1bde45ee71319401
Markus Tervonen <markus.tervonen@hotmail.com>
2025-09-10 11:38:24 +0300

    teht 8.10

--- osa8/library-frontend/src/queries.js
+++ osa8/library-frontend/src/queries.js
@@ -19,3 +19,24 @@ export const ALL_BOOKS = gql`
     }
   }
 `;
+
+export const ADD_BOOK = gql`
+  mutation Mutation(
+    $title: String!
+    $published: Int!
+    $author: String!
+    $genres: [String!]!
+  ) {
+    addBook(
+      title: $title
+      published: $published
+      author: $author
+      genres: $genres
+    ) {
+      author
+      genres
+      published
+      title
+    }
+  }
+`;
