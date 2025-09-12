3084c64b8893d66297b4c5408954df5a11e004ca
Markus Tervonen <markus.tervonen@hotmail.com>
2025-09-12 14:48:28 +0300

    teht 8.19

--- osa8/library-frontend/src/components/Books.jsx
+++ osa8/library-frontend/src/components/Books.jsx
@@ -1,17 +1,28 @@
 import { useQuery } from "@apollo/client/react";
 import { ALL_BOOKS } from "../queries";
+import { useState } from "react";
 
 const Books = (props) => {
+  const [selectedGenre, setSelectedGenre] = useState(null);
+  const books = useQuery(ALL_BOOKS);
+
   if (!props.show) {
     return null;
   }
-
-  const books = useQuery(ALL_BOOKS);
+  
   if (books.loading) {
     return <div>loading...</div>;
   }
 
   console.log(books)
+
+  const genres = Array.from(
+    new Set(books.data.allBooks.flatMap((book) => book.genres))
+  );
+
+  const filteredBooks = selectedGenre
+    ? books.data.allBooks.filter((book) => book.genres.includes(selectedGenre))
+    : books.data.allBooks;
 
   return (
     <div>
@@ -23,7 +34,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.data.allBooks.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
@@ -32,6 +43,14 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      <div>
+        <button onClick={() => setSelectedGenre(null)}>all genres</button>
+        {genres.map((genre) => (
+          <button key={genre} onClick={() => setSelectedGenre(genre)}>
+            {genre}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
