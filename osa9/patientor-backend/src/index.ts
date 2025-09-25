108955f010071006f37a53a706d7eeaa9c859666
Markus Tervonen <markus.tervonen@hotmail.com>
2025-09-25 12:04:15 +0300

    teht 9.10

---
+++ osa9/patientor-backend/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+import diagnosisRouter from "./routes/diagnoses";
+
+const PORT = 3001;
+
+app.get("/api/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
+
+app.use("/api/diagnosis", diagnosisRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
